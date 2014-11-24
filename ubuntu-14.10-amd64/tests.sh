e4d9b71066ed4a13ff380f9298bf163c5f44cfa3
Vladimir Timofeev <vovkasm@gmail.com>
2014-11-24 10:04:20 +0300

    Use ninja to build. Try speedup build by set VM number of cpus to 2.

--- ubuntu-14.10-amd64/tests.sh
+++ ubuntu-14.10-amd64/tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y build-essential cmake
+sudo apt-get install -y build-essential cmake ninja
 bash /vagrant/cocos2d-x/build/install-deps-linux.sh
 
-mkdir -p build && cd build && cmake /vagrant/cocos2d-x && make
+mkdir -p build && cd build && cmake -GNinja /vagrant/cocos2d-x && ninja -v
