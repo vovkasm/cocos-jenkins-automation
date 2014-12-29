e8a93a8aad226ee7860271cfdefef425ceae64c9
Vladimir Timofeev <vovkasm@gmail.com>
2014-12-30 01:09:31 +0300

    remove old stuff

--- run-tests.bat
+++
@@ -1,2 +0,0 @@
-set PATH=C:\msys64\usr\bin;%PATH%
-bash automation\run-tests.sh %1
--- ubuntu-14.10-amd64/tests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-sudo apt-get update
-sudo apt-get install -y build-essential cmake
-bash /vagrant/cocos2d-x/build/install-deps-linux.sh
-
-mkdir -p build && cd build && cmake /vagrant/cocos2d-x && make -j2
